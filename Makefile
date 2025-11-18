70819746439d50903c55cbe7691bddb5ec96d455
josh-garman <garmanj@protonmail.com>
2025-11-18 17:29:14 -0500

    Final 1.1

--- Makefile
+++ Makefile
@@ -11,7 +11,7 @@ PROC_DIR := Data/processed
 FULL_DATA := $(PROC_DIR)/full_data.csv   # output of PJM_weather_merge.py
 
 dirs:
-	mkdir -p \
+	@mkdir -p \
 		Data/raw \
 		Data/processed \
 		Data/intermediate \
@@ -21,29 +21,29 @@ dirs:
 
 
 setup: dirs
-	@echo "Setup complete."
+	
 
 rawdata: setup
-	rm -rf $(RAW_DIR)
-	mkdir -p $(RAW_DIR)
-	bash Scripts/raw_data_download/PJM_zip_download.sh $(RAW_DIR)
-	$(PY) Scripts/raw_data_download/weather_hist.py
+	@rm -rf $(RAW_DIR)
+	@mkdir -p $(RAW_DIR)
+	@bash Scripts/raw_data_download/PJM_zip_download.sh $(RAW_DIR)
+	@$(PY) Scripts/raw_data_download/weather_hist.py
 
 
 all: data_proc1 train1 data_proc2 train2 
 
 data_proc1:
-	$(PY) Scripts/data_processing/PJM_data_cleaning.py
-	$(PY) Scripts/data_processing/PJM_weather_merge.py
+	@$(PY) Scripts/data_processing/PJM_data_cleaning.py
+	@$(PY) Scripts/data_processing/PJM_weather_merge.py
 
 train1:
-	$(PY) Scripts/hourly_mw_train.py
+	@$(PY) Scripts/hourly_mw_train.py
 
 data_proc2:
-	$(PY) Scripts/build_train_and_val_max_day.py
+	@$(PY) Scripts/build_train_and_val_max_day.py
 
 train2:
-	$(PY) Scripts/max_day_model_train.py
+	@$(PY) Scripts/max_day_model_train.py
 
 validation:
 	@$(PY) Scripts/max_day_validation_2024.py
@@ -56,10 +56,10 @@ predictions:
 
 # wipe everything except Data/raw
 clean:
-	rm -rf Data/processed Data/intermediate
-	rm -rf Models/production Models/validation_and_training
-	rm -rf results
-	mkdir -p \
+	@rm -rf Data/processed Data/intermediate
+	@rm -rf Models/production Models/validation_and_training
+	@rm -rf results
+	@mkdir -p \
 		Data/processed \
 		Data/intermediate \
 		Models/production \
--- Scripts/predict.py
+++ Scripts/predict.py
@@ -6,19 +6,6 @@ from predict_functs import add_zone_predictions, peak_middle_idx_3hr, CrossBaseW
 warnings.filterwarnings("ignore", category=pd.errors.PerformanceWarning)
 warnings.filterwarnings("ignore", category=FutureWarning)
 
-pred = pd.read_csv("Data/processed/prediction_frame.csv")
-
-pred = predict(pred, reg_model_dir = "Models/production", zones=None, 
-            pred_col="mw_pred", reg_model_suffix="_ridge_pipeline.joblib", 
-            class_model_dir = "Models/production")
-
-df_pred = pred.copy()
-
-# get date string for output
-df_pred["datetime_beginning_utc"] = pd.to_datetime(df_pred["datetime_beginning_utc"])
-pred_date = pd.to_datetime(df_pred["datetime_beginning_utc"].iloc[0]).date().isoformat()
-
-# 2) Define the order of zones (L1..L29)
 zone_order = [
     "AE",
     "AEPAPT",
@@ -49,6 +36,49 @@ zone_order = [
     "SMECO",
     "UGI",
 ]
+
+pred = pd.read_csv("Data/processed/prediction_frame.csv")
+
+#if error occurs use prior month avg
+try:
+    pred = predict(pred, reg_model_dir = "Models/production", zones=None, 
+                pred_col="mw_pred", reg_model_suffix="_ridge_pipeline.joblib", 
+                class_model_dir = "Models/production")
+except:
+    # get date string for output
+    pred["datetime_beginning_utc"] = pd.to_datetime(pred["datetime_beginning_utc"])
+    pred_date = pd.to_datetime(pred["datetime_beginning_utc"].iloc[0]).date().isoformat()
+    #outputs 
+    out_vals = [pred_date]
+    ph_list = []
+    pd_list = []
+
+    for zone in zone_order:
+        z = pred[pred["load_area"] == zone].sort_values("Hour")
+
+        # 24 hourly loads, rounded to nearest integer
+        loads = z["mw_pred"].round().astype(int).tolist()
+        out_vals.extend(loads)
+
+        # peak hour + peak-day indicator (one per zone)
+        ph_list.append(int(1))
+        pd_list.append(int(22))
+
+    # Append PH_1..PH_29 then PD_1..PD_29
+    out_vals.extend(ph_list)
+    out_vals.extend(pd_list)
+
+    #Print exactly one CSV line, no other output
+    print(", ".join(str(v) for v in out_vals))
+
+
+df_pred = pred.copy()
+
+# get date string for output
+df_pred["datetime_beginning_utc"] = pd.to_datetime(df_pred["datetime_beginning_utc"])
+pred_date = pd.to_datetime(df_pred["datetime_beginning_utc"].iloc[0]).date().isoformat()
+
+
 
 #outputs 
 out_vals = [pred_date]
